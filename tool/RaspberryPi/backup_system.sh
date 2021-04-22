aafdc3e8db07dd31fbe88347a8ed5d9db9ec4a02
Neucrack <CZD666666@gmail.com>
2021-04-23 01:20:00 +0800

    temperary commit

--- tool/RaspberryPi/backup_system.sh
+++ tool/RaspberryPi/backup_system.sh
@@ -1,29 +1,46 @@
+
+
+
 #!/bin/bash
+set -o errexit
 ######################################################
 ################## TODO: settings#####################
-src_root_device=/dev/sdc2 #/dev/root
-src_boot_device=/dev/sdc1 #/dev/mmcblk0p1
+src_root_device=/dev/root #/dev/root
+src_boot_device=/dev/mmcblk0p1 #/dev/mmcblk0p1
+backup_on_pi=1                 # 1: backup on pi, 0: backuo on PC
 ######################################################
 
 green="\e[32;1m"
 normal="\e[0m"
 
+echo -e "before backup, it's better to clean your temp files and not useful files, and carefully, not detele useful data!!!"
+if [[ "${backup_on_pi}x" == "1x" ]]; then
+  echo -e "${gren} \nclean apt cache\n${normal}"
+  sudo apt-get autoremove -yq --purge
+  sudo apt-get clean
+  sudo rm -rf /var/lib/apt/lists/*
+  sudo rm -rf /tmp/*
+fi
+
+
 echo -e "${green} \ninstall software\n ${normal}"
-sudo apt-get install -y dosfstools dump parted kpartx
+sudo apt-get install -y dosfstools dump parted kpartx bc
 echo -e "${green} \ninstall software complete\n ${normal}"
 
 echo -e "${green}create image now\n ${normal}"
 used_size=`df -P | grep $src_root_device | awk '{print $3}'`
 boot_size=`df -P | grep $src_boot_device | awk '{print $2}'`
 if [ "x${used_size}" != "x" ] && [ "x${boot_size}" != "x" ];then
-	count=`echo "${used_size}*1.1+${boot_size}+2"|bc|awk '{printf("%.0f",$1)}'`
+        count=`echo "${used_size}*1.1+${boot_size}+2"|bc|awk '{printf("%.0f",$1)}'`
 else
-	echo "device $src_root_device or $src_boot_device not exist,mount first"
-	exit 0;
+        echo "device $src_root_device or $src_boot_device not exist,mount first"
+        exit 0;
 fi
 echo boot size:$boot_size,used_size:$used_size,block count: $count
-echo $(($boot_size/1024+1))
+echo "boot size $(($boot_size/1024+1))KiB"
+echo "${green} now generate empty img, it may take a while, wait please ... ${normal}"
 sudo dd if=/dev/zero of=backup.img bs=1k count=$count
+echo "${green} now part img ${normal}"
 sudo parted backup.img --script -- mklabel msdos
 sudo parted backup.img --script -- mkpart primary fat32 1M $(($boot_size/1024+1))M #(nByte/512)s
 sudo parted backup.img --script -- mkpart primary ext4 $(($boot_size/1024+1))M -1
@@ -43,15 +60,19 @@ sleep 2
 sudo mkfs.vfat $boot_device
 sudo mkfs.ext4 $root_device
 sudo mkdir -p /media/img_to
-sudo mkdir /media/img_src
+sudo mkdir -p /media/img_src
 mount_path=`df -h|grep ${src_boot_device}|awk '{print $6}'`
 if [ "x${mount_path}" == "x" ];then
   sudo mount -t vfat $src_boot_device /media/img_src
   mount_path=/media/img_src
 fi
 sudo mount -t vfat $boot_device /media/img_to
-echo -e "${green}copy /boot${normal}"
-sudo cp -rfp ${mount_path}/* /media/img_to
+echo -e "${green}copy /boot(${mount_path} to /media/img_to)${normal}"
+if [[ "${backup_on_pi}x" == "1x" ]]; then
+  sudo cp -rf ${mount_path}/* /media/img_to
+else
+  sudo cp -rfp ${mount_path}/* /media/img_to
+fi
 sudo umount /media/img_to
 
 sudo chattr +d backup.img #exclude img file from backup(support in ext* file system)
